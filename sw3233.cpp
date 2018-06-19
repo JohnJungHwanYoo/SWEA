35108510d9778b824697657df7ce04fd3dcce608
JohnJungHwanYoo <johnyoo90@hotmail.com>
2018-06-20 00:33:40 +0900

    Create sw3233.cpp
    //3233. 정삼각형 분할 놀이

---
+++ sw3233.cpp
@@ -0,0 +1,19 @@
+//3233. 정삼각형 분할 놀이
+
+#include<stdio.h>
+
+int test_case;
+
+int main(void)
+{
+	scanf("%d", &test_case);
+	for (int i = 0; i < test_case; i++)
+	{
+		long long A, B;
+		scanf("%lld %lld", &A, &B);
+		long long quot = A / B;
+		printf("#%d %lld\n", i + 1, quot*quot);
+
+	}
+	return 0;
+}
