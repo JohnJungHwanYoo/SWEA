ad25a01eeaa01170aeeccfdb45cd974afa317ea2
JohnJungHwanYoo <johnyoo90@hotmail.com>
2018-07-02 16:15:18 +0900

    // 1953. [모의 SW 역량테스트] 탈주범 검거
    // 1953. [모의 SW 역량테스트] 탈주범 검거

--- sw1953.cpp
+++ sw1953.cpp
@@ -110,3 +110,121 @@ int main(void)
     }
     return 0;
 }
+
+//다른 사람 풀이
+/*
+
+#include <iostream>
+#include <queue>
+using namespace std;
+ 
+int d[50][50];
+int dist[50][50];
+int result;
+int N, M, R, C, L;
+bool check[50][50];
+void go(int L, int i, int j);
+ 
+int main() {
+    int T;
+    scanf("%d", &T);
+    for (int t = 1; t <= T; t++) {
+        scanf("%d %d %d %d %d", &N, &M, &R, &C, &L);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                scanf("%d", &d[i][j]);
+            }
+        }
+        result = 0;
+        go(L, R, C);
+        printf("#%d %d\n", t, result);
+ 
+        if (t != T) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    check[i][j] = false;
+                    dist[i][j] = 0;
+                }
+            }
+        }
+    }
+    return 0;
+}
+ 
+bool checkUp(int i, int j) {
+    if ((i > 0 && !check[i - 1][j]) && (d[i - 1][j] == 1 || d[i - 1][j] == 2 || d[i - 1][j] == 5 || d[i - 1][j] == 6)) {
+        dist[i - 1][j] = dist[i][j] + 1;
+        return true;
+    }
+    return false;
+}
+ 
+bool checkDown(int i, int j) {
+    if ((i < N-1 && !check[i + 1][j]) && (d[i + 1][j] == 1 || d[i + 1][j] == 2 || d[i + 1][j] == 4 || d[i + 1][j] == 7)) {
+        dist[i + 1][j] = dist[i][j] + 1;
+        return true;
+    }
+    else return false;
+}
+ 
+bool checkLeft(int i, int j) {
+    if ((j > 0 && !check[i][j - 1]) && (d[i][j - 1] == 1 || d[i][j - 1] == 3 || d[i][j - 1] == 4 || d[i][j - 1] == 5)) {
+        dist[i][j - 1] = dist[i][j] + 1;
+        return true;
+    }
+    else return false;
+}
+ 
+bool checkRight(int i, int j) {
+    if ((j < M-1 && !check[i][j + 1]) && (d[i][j + 1] == 1 || d[i][j + 1] == 3 || d[i][j + 1] == 6 || d[i][j + 1] == 7)) {
+        dist[i][j + 1] = dist[i][j] + 1;
+        return true;
+    }
+    else return false;
+}
+ 
+void go(int Lvar, int i, int j) {
+    queue<pair<int, int>> q;
+    q.push(make_pair(i, j));
+    dist[i][j] = 1;
+    while (!q.empty()) {
+        int ni = q.front().first; int nj = q.front().second;
+        q.pop();
+        if (!check[ni][nj] && dist[ni][nj] <= Lvar) {
+            if (d[ni][nj] == 1) {
+                if (checkUp(ni, nj)) q.push(make_pair(ni - 1, nj));
+                if (checkDown(ni, nj)) q.push(make_pair(ni + 1, nj));
+                if (checkLeft(ni, nj)) q.push(make_pair(ni, nj - 1));
+                if (checkRight(ni, nj)) q.push(make_pair(ni, nj + 1));
+            }
+            else if (d[ni][nj] == 2) {
+                if (checkUp(ni, nj)) q.push(make_pair(ni - 1, nj));
+                if (checkDown(ni, nj)) q.push(make_pair(ni + 1, nj));
+            }
+            else if (d[ni][nj] == 3) {
+                if (checkLeft(ni, nj)) q.push(make_pair(ni, nj - 1));
+                if (checkRight(ni, nj)) q.push(make_pair(ni, nj + 1));
+            }
+            else if (d[ni][nj] == 4) {
+                if (checkUp(ni, nj)) q.push(make_pair(ni - 1, nj));
+                if (checkRight(ni, nj)) q.push(make_pair(ni, nj + 1));
+            }
+            else if (d[ni][nj] == 5) {
+                if (checkDown(ni, nj)) q.push(make_pair(ni + 1, nj));
+                if (checkRight(ni, nj)) q.push(make_pair(ni, nj + 1));
+            }
+            else if (d[ni][nj] == 6) {
+                if (checkLeft(ni, nj)) q.push(make_pair(ni, nj - 1));
+                if (checkDown(ni, nj)) q.push(make_pair(ni + 1, nj));
+            }
+            else if (d[ni][nj] == 7) {
+                if (checkUp(ni, nj)) q.push(make_pair(ni - 1, nj));
+                if (checkLeft(ni, nj)) q.push(make_pair(ni, nj - 1));
+            }
+ 
+            result++;
+            check[ni][nj] = true;
+        }
+    }
+}
+*/
